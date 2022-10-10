1c5147888931dbdcb530eefbe24912ef2bf2ada8
Ermiyas  Hailemichael <yemeerma11@gmail.com>
2022-10-10 02:05:24 -0700

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.get('/greeting', (req, res)=>{
     res.send('Hello, strangers')
 })
 // Give the greetingroute a param /:name
+// Give the greetingroute a param /:name
  app.get('/greeting/:Jimmy_boy',(req, res)=>{
     res.send("Wow! Hello there, " + "  " + req.params.Jimmy_boy + " on the page")
  })
@@ -22,6 +23,44 @@ app.get('/tip/:total/:tipPercentage',(req, res)=>{
     let tip = parseInt(req.params.total) * (parseInt(req.params.tipPercentage) / 100)
     res.send(`${tip}`+ " is the tip you will give.")
 })
+// Magic 8 Ball
+// Create a route of '/magic'that should expect a phrase in the 
+// URL that ask the Magic 8 ball a question.
+
+const magic8_quote = [
+    "It is certain", 
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it yes",
+    "Most likely", 
+    "Outlook good",
+    "Yes", 
+    "Signs point to yes", 
+    "Reply hazy try again", 
+    "Ask again later",
+    "Better not tell you now", 
+    "Cannot predict now", 
+    "Concentrate and ask again",
+    "Don't count on it", 
+    "My reply is no", 
+    "My sources say no",
+    "Outlook not so good", 
+    "Very doubtful"
+]
+
+app.get('/magic',(req, res)=>{
+
+    res.send("Enter a question in the URL space")
+})
+app.get("/magic/:whatever",(req, res)=>{
+    let random = Math.floor( Math.random()*(magic8_quote.length))
+    console.log(random)
+    res.send(req.params.whatever.replaceAll('%20 ',' ') + " " + `${magic8_quote[random]}`)
+})
+
+
 
 app.listen(3000,()=>{
     console.log(`listening on port 3000...`)
