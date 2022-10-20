e2be5d4a9ab178e7cd4e91081316aed9b7498a92
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-10-19 19:52:57 -0600

    add feedback

--- pass-it-around/passit.js
+++ pass-it-around/passit.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+//nice job connecting your / route to /:number_of_bottles
 app.get('/',(req, res)=>{
     res.send(`99 Bottles of beer on the wall
     <html>
@@ -14,7 +14,7 @@ app.get('/',(req, res)=>{
  app.get('/:number_of_bottles', (req, res)=>{
     parseInt(req.params.number_of_bottles) -1;
     let down = parseInt(req.params.number_of_bottles)-1;
-
+//great solution for starting the count over
     if(parseInt(req.params.number_of_bottles)===0){
         res.send(`99 bottle of beer on the wall
         <html>
